56a6bceb1c9a14a3ee1f139ab1046003b17b1ebc
Chris Yeo <yeochinyi@gmail.com>
2020-01-11 21:49:28 +0800

    ok

--- exercism/c/armstrong-numbers/src/armstrong_numbers.c
+++ exercism/c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,5 +1,7 @@
 #include "armstrong_numbers.h"
 
+#include <stdio.h>      /* printf, fgets */
+#include <stdlib.h>     /* atoi */
 #include <math.h>
 #include <string.h>
 
@@ -11,8 +13,33 @@
 // 153 is an Armstrong number, because: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
 // 154 is not an Armstrong number, because: 154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
 int is_armstrong_number(int candidate){
-    int size = (int)((ceil(log10(candidate))+1)*sizeof(char));
-    char a[size];
-    sprintf(a, "%d", candidate);
-    return 1;
+
+    // int digits = (int)((ceil(log10(candidate))+1)*sizeof(char)); //cannot work in windows
+
+    int digits=0;
+    int test=candidate;
+    while(1){
+        test/=10;
+        digits++;
+        if(test==0){
+            break;
+        }
+    }
+
+    printf("digits=%d\n",digits);
+
+    int total=0;
+    for(int i=0; i<digits; i++){
+        int remain=candidate%(10^i);
+        printf("remain=%d\n",remain);
+        total+=pow(remain,digits);
+    }
+    printf("total=%d\n",total);
+    return total==candidate;
 }
+
+int main (int argc, char *argv[]){
+    int i=atoi(argv[1]);
+    printf("%d is_armstrong_number=%d\n",i, is_armstrong_number(i));
+}
+
