5cd75963d1e02f1f0264961abbd25799b32c7949
Chris Yeo <yeochinyi@gmail.com>
2020-01-11 22:06:29 +0800

    ok

--- exercism/c/armstrong-numbers/src/armstrong_numbers.c
+++ exercism/c/armstrong-numbers/src/armstrong_numbers.c
@@ -14,32 +14,27 @@
 // 154 is not an Armstrong number, because: 154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190
 int is_armstrong_number(int candidate){
 
-    // int digits = (int)((ceil(log10(candidate))+1)*sizeof(char)); //cannot work in windows
-
-    int digits=0;
-    int test=candidate;
-    while(1){
-        test/=10;
-        digits++;
-        if(test==0){
-            break;
-        }
+    int each_digits[10];
+    int num_digits=0;
+    int clone=candidate;
+    while(clone!=0){
+        int remain=clone%10;
+        each_digits[num_digits]=remain;
+        clone/=10;
+        num_digits++;
     }
-
-    printf("digits=%d\n",digits);
 
     int total=0;
-    for(int i=0; i<digits; i++){
-        int remain=candidate%(10^i);
-        printf("remain=%d\n",remain);
-        total+=pow(remain,digits);
+    for(int i=0; i<num_digits;i++){
+        total+=pow(each_digits[i],num_digits);
     }
-    printf("total=%d\n",total);
+
     return total==candidate;
 }
 
-int main (int argc, char *argv[]){
-    int i=atoi(argv[1]);
-    printf("%d is_armstrong_number=%d\n",i, is_armstrong_number(i));
-}
+// int main (int argc, char *argv[]){
+//     (void) argc; //get rid of compiler warning
+//     int i=atoi(argv[1]);
+//     printf("%d is_armstrong_number=%d\n",i, is_armstrong_number(i));
+// }
 
