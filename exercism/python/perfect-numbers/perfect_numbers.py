3005d84d1abe9a724b74b9e52eedd4ad3a1750be
Chris Yeo <yeochinyi@gmail.com>
2020-01-29 00:04:39 +0800

    ok

--- exercism/python/perfect-numbers/perfect_numbers.py
+++ exercism/python/perfect-numbers/perfect_numbers.py
@@ -2,10 +2,10 @@ import sys
 
 def classify(number):
     ''' return perfect, abundant, or deficient if number is matching aliquot sum '''
-    
+
     num = int(number)
     if num < 1:
-        raise ValueError("number is invalid")
+        raise ValueError(f"number {num} is invalid")
     aliquot = sum([i for i in range(1,num) if num%i==0])
     if aliquot == number:
         return "perfect"
