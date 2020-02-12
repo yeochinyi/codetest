3bebc2a2068466e0efb9176de3ddcb0f0dcc5991
Chris Yeo <yeochinyi@gmail.com>
2020-02-12 21:05:31 +0800

    ok

--- exercism/go/protein-translation/protein_translation.go
+++ exercism/go/protein-translation/protein_translation.go
@@ -1,24 +1,65 @@
 package protein
 
-var (
-	// ErrStop : stop error
-	ErrStop = constError("ErrStop")
-	// ErrInvalidBase : stop error
-	ErrInvalidBase = constError("ErrInvalidBase")
-)
+import "errors"
 
-type constError string
+// ErrStop : stop protein
+var ErrStop = errors.New("Encountered STOP protein")
 
-func (err constError) Error() string {
-	return string(err)
-}
+// ErrInvalidBase : invalid string
+var ErrInvalidBase = errors.New("Encountered invalid base")
 
-// FromCodon from
+// var (
+// 	// ErrStop : stop protein
+// 	ErrStop = constError("ErrStop")
+
+// 	// ErrInvalidBase : invalid string
+// 	ErrInvalidBase = constError("ErrInvalidBase")
+// )
+
+// // for creation of const Error string
+// type constError string
+
+// func (err constError) Error() string {
+// 	return string(err)
+// }
+
+// FromCodon translate condon to protein
 func FromCodon(input string) (output string, e error) {
-	return "Methionine", nil
+	switch input {
+	case "AUG":
+		return "Methionine", nil
+	case "UUU", "UUC":
+		return "Phenylalanine", nil
+	case "UUA", "UUG":
+		return "Leucine", nil
+	case "UCU", "UCC", "UCA", "UCG":
+		return "Serine", nil
+	case "UAU", "UAC":
+		return "Tyrosine", nil
+	case "UGU", "UGC":
+		return "Cysteine", nil
+	case "UGG":
+		return "Tryptophan", nil
+	case "UAA", "UAG", "UGA":
+		return "", ErrStop
+	default:
+		return "", ErrInvalidBase
+	}
 }
 
-// FromRNA from
-func FromRNA(input string) (output []string, e error) {
-	return []string{"Methionine", "Phenylalanine", "Serine"}, nil
+// FromRNA translate condon combo to protein array
+func FromRNA(input string) ([]string, error) {
+	var output []string
+	for i := 0; i < len(input); i += 3 {
+		c, err := FromCodon(input[i : i+3])
+		if err != nil {
+			if err == ErrStop {
+				err = nil
+			}
+			return output, err
+		}
+		output = append(output, c)
+	}
+
+	return output, nil
 }
