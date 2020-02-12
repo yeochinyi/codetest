71a6b8ecef88c90c8f6153b929f1f8dc04ffc16c
Chris Yeo <christopher.yeo@sc.com>
2020-02-12 12:48:28 +0800

    ok

--- exercism/python/high-scores/high_scores.py
+++ exercism/python/high-scores/high_scores.py
@@ -1,17 +1,20 @@
-import functools 
 import ast
 import sys
 
+# return latest score
 def latest(scores):
     return scores[-1]
 
+# return top score
 def personal_best(scores):
-    return functools.reduce(lambda a,b: a if a > b else b, scores)
+    return max(scores)
 
+# return top 3 scores
 def personal_top_three(scores):
     return sorted(scores, reverse=True)[:3]
 
-if __name__ == "__main__":
-    # python .\high_scores.py "[1,2,3]"
-    inp = ast.literal_eval(sys.argv[1])
-    print(personal_top_three(inp))
+# For testing
+# if __name__ == "__main__":
+#     # python .\high_scores.py "[1,2,3]"
+#     inp = ast.literal_eval(sys.argv[1])
+#     print(personal_top_three(inp))
