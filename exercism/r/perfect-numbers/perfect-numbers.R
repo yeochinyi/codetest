f7004c07a9e92af7446643be7c88a9fb1443ec1e
Chris Yeo <yeochinyi@gmail.com>
2020-02-16 21:29:36 +0800

    ok

--- exercism/r/perfect-numbers/perfect-numbers.R
+++ exercism/r/perfect-numbers/perfect-numbers.R
@@ -1,5 +1,6 @@
 # returns perfect, abundant, or deficient depending on n
 number_type <- function(n){
+    stopifnot(n > 0)
     if(n == 1) return ("deficient")
     vec <- 1:(n-1)
     total <- sum(vec[n%%vec==0])
