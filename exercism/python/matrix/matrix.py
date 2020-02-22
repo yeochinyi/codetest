b1cd21e1539be74dc30b6b71adcc0f77de0fbab0
Chris Yeo <yeochinyi@gmail.com>
2020-02-22 13:35:57 +0800

    ok

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -1,21 +1,22 @@
 class Matrix:
     """ create a matrix starting from 1 as base  """
     def __init__(self, matrix_string):
-        self.matrix = list(
-            map(
-                lambda x: list(map(
-                    lambda y: int(y), 
-                    x.split(" ")
-                )),
-                matrix_string.split("\n")
-            )
-        )
+        # self.matrix = list(
+        #     map(
+        #         lambda x: list(map(
+        #             lambda y: int(y), 
+        #             x.split(" ")
+        #         )),
+        #         matrix_string.split("\n")
+        #     )
+        # )
+        self.matrix = [[ int(y) for y in x.split(" ")] for x in matrix_string.split("\n")]    
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        return list(map(lambda x: x[index - 1], self.matrix))
-
+        # return list(map(lambda x: x[index - 1], self.matrix))
+        return [x[index - 1] for x in self.matrix]
 
 if __name__ == "__main__":
     print(Matrix("1 2\n3 4").column(1))
