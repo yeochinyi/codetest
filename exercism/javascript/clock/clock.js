20a57bebead15be17c8943c9afb516582272e46a
Chris Yeo <yeochinyi@gmail.com>
2020-02-25 22:41:14 +0800

    ok

--- exercism/javascript/clock/clock.js
+++ exercism/javascript/clock/clock.js
@@ -1,26 +1,48 @@
-//
-// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(h, m=0) {
+    this.setTime(h,m)    
+  }
+
+  setTime(h,m){
+    var minToHour = parseInt( m / 60 )
+    this.min = m % 60
+    if(this.min < 0){
+      this.min += 60
+      minToHour -= 1
+    }
+
+    this.hour = (h + minToHour) % 24
+    if(this.hour < 0){
+      this.hour = 24 + this.hour
+    }
+  }
+
+  getNumberDisplay(number){
+    // if(number < 10){
+    //   return  "0" + number
+    // }
+
+    // return "" + number
+    return number.toString().padStart(2, '0')
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    var hourString = this.getNumberDisplay(this.hour)
+    var minString = this.getNumberDisplay(this.min)
+    return `${hourString}:${minString}`
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(m) {
+    this.setTime(this.hour, this.min + m)
+    return this
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(m) {
+    this.setTime(this.hour, this.min - m)
+    return this
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  equals(clock) {
+    return clock.hour === this.hour && clock.min === this.min
   }
 }
