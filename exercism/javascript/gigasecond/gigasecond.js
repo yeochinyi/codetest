c9336398f874b320ccd333fc17cb771baba6d878
Chris Yeo <yeochinyi@gmail.com>
2020-02-25 20:10:47 +0800

    ok

--- exercism/javascript/gigasecond/gigasecond.js
+++ exercism/javascript/gigasecond/gigasecond.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const GIGASEC = 1e12
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (date) => {
+  return new Date(date.getTime() + GIGASEC)
 };
