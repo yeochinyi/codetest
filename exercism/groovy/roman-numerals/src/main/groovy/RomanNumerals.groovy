ce526599e9a18e12a1611aa49dee4f4a5c258561
Chris Yeo <yeochinyi@gmail.com>
2020-02-28 21:28:54 +0800

    ok

--- exercism/groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
+++ exercism/groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
@@ -1,4 +1,8 @@
 class RomanNumerals {
+    //  I=1, V=5, 
+    // X=10, L=50, 
+    // C=100, D=500, 
+    // M=1K
     RomanNumerals() {
         Integer.metaClass.getRoman = { ->
             // RETURN the roman representation of an Integer here
---
+++ exercism/python/hamming/hamming.py
@@ -0,0 +1,13 @@
+import functools 
+
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError(f"strands are not equal")
+    zipped = zip(strand_a, strand_b)
+
+    # return functools.reduce(lambda acc, x : acc + (1 if x[0] != x[1] else 0) , zipped, 0)
+    return sum(i!=j for i,j in zip(strand_a, strand_b))
+
+
+# if __name__ == "__main__":
+#     print(distance("GGACGGATTCTG", "AGGACGGATTCT"))
