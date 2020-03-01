c10abe1b738c3ad91bf1edd177d8bb14e6d8a0ab
Chris Yeo <yeochinyi@gmail.com>
2020-03-01 21:22:53 +0800

    ok

--- exercism/groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
+++ exercism/groovy/roman-numerals/src/main/groovy/RomanNumerals.groovy
@@ -3,9 +3,36 @@ class RomanNumerals {
     // X=10, L=50, 
     // C=100, D=500, 
     // M=1K
+
+    // last 2 chars are 'blanks'
+    def digits = "IVXLCDM--"
+
     RomanNumerals() {
         Integer.metaClass.getRoman = { ->
-            // RETURN the roman representation of an Integer here
+
+            def ret = ""
+            delegate.toString().reverse().eachWithIndex { v, i ->
+                def (ones, fives, tens) = [0,1,2].collect{ digits.charAt(i*2+it).toString() }
+                def text = ""
+                switch(v as int){
+                    case 1..3: text = ones * val
+                    break
+                    case 4..5: text = ones * (5-val) + fives
+                    break
+                    case 6..8: text = fives + ones * (val-5)
+                    break
+                    case 9: text =  ones * (10-val) + tens
+                }
+                ret = text + ret
+            }
+            return ret
+
+            
         }
+    }
+
+    static main(args){
+        new RomanNumerals()
+        println 2856.roman
     }
 }
